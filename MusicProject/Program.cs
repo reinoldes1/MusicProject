0f16fd960858c18a1bfe9cb23064424a0bb22083
João Victor <reinoldesxbox@gmail.com>
2024-05-25 14:39:45 -0400

    Adicionar arquivos de projeto.

---
+++ MusicProject/Program.cs
@@ -0,0 +1,26 @@
+﻿// C# Training
+
+Song song1 = new Song();
+song1.name = "Doomed";
+song1.artist = "Bring me the Horizon";
+song1.duration = 360;
+song1.available = true;
+
+Song song2 = new Song();
+song2.name = "The Summoning";
+song2.artist = "Sleep Token";
+song2.duration = 243;
+song2.available = false;
+
+Song song3 = new Song();
+song3.name = "Custer";
+song3.artist = "Slipknot";
+song3.duration = 275;
+song3.available = true;
+
+song1.ShowTrackDetails();
+Console.WriteLine("\n");
+song2.ShowTrackDetails();
+Console.WriteLine("\n");
+song3.ShowTrackDetails();
+Console.WriteLine("\n");
---
+++ MusicProject/Song.cs
@@ -0,0 +1,16 @@
+﻿class Song 
+{
+    public string name;
+    public string artist;
+    public int duration;
+    public bool available;
+
+    public void ShowTrackDetails()
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Artist: {artist}");
+        Console.WriteLine($"Duration: {duration}");
+        if (available) Console.WriteLine("Available to listen");
+        else Console.WriteLine("Not available to listen at the moment");
+    }
+}
